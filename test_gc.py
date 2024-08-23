d7990940e0c801f431d4ea83e3735018debe1701
GKNB <tw2507@columbia.edu>
2024-08-23 10:52:27 -0700

    Use file content as argument to resolve remote issue

--- test_gc.py
+++ test_gc.py
@@ -1,10 +1,19 @@
 from globus_compute_sdk import Executor
 
-def submit_ATLAS_template():
+def get_file_content(filename):
+    with open(filename, 'r') as file:
+        file_content = file.read()
+    return file_content
+
+
+def submit_ATLAS_template(file_content):
+#    print(f"file_content = \n{file_content}\n\n\n")
+
     import subprocess
 #    result = subprocess.run(['srun', '-n', '8', '--ntasks-per-node=4', '--cpus-per-task=64', 'check-mpi.gnu.pm'], capture_output=True, text=True)
 #    result = subprocess.run(['srun', '-n', '8', '--ntasks-per-node=4', '--cpus-per-task=64', '/bin/bash', '/global/homes/t/tianle/myWork/aid2e/globus_compute/test/test_ALTAS_toy/wrapper-test.sh'], capture_output=True, text=True)
-    result = subprocess.run(['/bin/bash', '/global/homes/t/tianle/myWork/aid2e/globus_compute/test/test_ALTAS_toy/atlas_srun_command.sh'], capture_output=True, text=True)
+
+    result = subprocess.run(['/bin/bash', '-c', file_content], capture_output=True, text=True)
     return result.stdout, result.stderr
 
 #def submit_ATLAS_bash_template():
@@ -27,11 +36,13 @@ def submit_ATLAS_template():
 
 
 tutorial_endpoint_id = '733ef4d2-4e13-409d-9e30-c6955b4d209d'
+file_content = get_file_content('/global/homes/t/tianle/myWork/aid2e/globus_compute/test/test_ALTAS_toy/atlas_srun_command.sh')
+#file_content = get_file_content('/global/homes/t/tianle/myWork/aid2e/globus_compute/test/test_ALTAS_toy/my_srun_command.sh')
 
 # ... then create the executor, ...
 with Executor(endpoint_id=tutorial_endpoint_id) as gce:
     # ... then submit for execution, ...
-    future = gce.submit(submit_ATLAS_template)
+    future = gce.submit(submit_ATLAS_template, file_content)
     print("Job submitted!")
     # ... and finally, wait for the result
     print("Out: ", future.result()[0])
